b005d4140622f2eeea8ac3a478c2a9424b51c758
tactico <lcheung@mylo.ai>
2022-11-14 20:11:37 -0500

    Refactor to resolvers and schema

---
+++ server/resolvers/user.ts
@@ -0,0 +1,39 @@
+export const resolvers = {
+  Query: {
+    async users(root, args, { models }) {
+      return models.User.findAll();
+    },
+    async userById(root, { id }, { models }) {
+      return models.User.findByPk(id);
+    },
+  },
+  Mutation: {
+    async createUser(root, { name, email, password }, { models }) {
+      return models.User.create({
+        name,
+        email,
+        password, //: await bcrypt.hash(password, 10),
+        isActive: true,
+      });
+    },
+    async updateUser(root, { id, name, email }, { models }) {
+      const user = await models.User.findByPk(id);
+      user.set({
+        name: name,
+        email: email,
+      });
+      return user.save();
+    },
+    async deleteUser(root, { id }, { models }) {
+      return models.User.destroy({
+        where: { id },
+      });
+    },
+  },
+  User: {
+    async courses(user) {
+      return user.getCourses();
+    },
+  },
+};
+
---
+++ server/schema/user.ts
@@ -0,0 +1,28 @@
+export const typeDefs = `
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+    password: String!
+    isActive: Boolean!
+    courses: [Course]!
+  }
+
+  extend type Query {
+    users: [User]!
+    userById(id: Int!): User    
+  }
+
+  extend type Mutation {
+    createUser(name: String!,
+      email: String!,
+      password: String!,
+      isActive: Boolean): User!
+    updateUser(id: Int!, 
+      name: String, 
+      email: String, 
+      password: String,
+      isActive: Boolean): User!
+    deleteUser(id: Int!): String  
+  }  
+`

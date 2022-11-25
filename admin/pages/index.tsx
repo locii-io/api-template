6be302a543182212566e49e19df757da1b2bd8ec
tactico <lcheung@mylo.ai>
2022-11-24 21:34:31 -0500

    Add next admin panel

---
+++ admin/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container, Typography } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography variant="h4">Hello world</Typography>
+    </Container>
+  );
+}
--- app.ts
+++ app.ts
@@ -1,12 +1,30 @@
 import createServer from "./server/server";
+import next from 'next';
 
 // Create Express server
 const port = 4000;
 const app = createServer();
 
-// Start Express server
-app.listen({ port }, () => {
-    console.log(`Started server on port: ${port}`);
-    console.log(`🚀 Apollo Server ready at http://localhost:${port}/graphql`);
-    console.log(`🚀 REST Server ready at http://localhost:${port}/api`);
-});
+const nextAdmin = next({
+    dev: process.env.NODE_ENV !== 'production',
+    dir: './admin',
+})
+const handle = nextAdmin.getRequestHandler()
+nextAdmin.prepare()
+    .then(() => {
+        app.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        // Start Express server
+        app.listen({ port }, () => {
+            console.log(`Started server on port: ${port}`);
+            console.log(`🚀 Apollo Server ready at http://localhost:${port}/graphql`);
+            console.log(`🚀 REST Server ready at http://localhost:${port}/api`);
+            console.log(`🚀 Admin Portal ready at http://localhost:${port}/admin/dashboard`);
+        });
+    })
+    .catch((ex) => {
+        console.error(ex.stack)
+        process.exit(1)
+    })

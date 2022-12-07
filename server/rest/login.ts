effc24b06b6f1b317d1e310efef5b781da1ecb0d
MejanH <muhammad.mejanul.haque@gmai.com>
2022-12-07 22:43:29 +0600

    feat!: remove anythings existing files

--- server/rest/login.ts
+++
@@ -1,9 +0,0 @@
-export const routes = {
-  'Mutation.login': {
-    method: 'POST',
-    path: '/login',
-    responseStatus: 200,
-    tags: ['Authentication'],
-    description: 'Login and get an authentication token.',
-  },
-};
